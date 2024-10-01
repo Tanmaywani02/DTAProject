735fe3fd0bf23c0af728fbee8ee9e64585d80b22
Tanmaywani02 <tanmaywani02@gmail.com>
2024-10-01 09:37:46 +0530

    header & footer added to forms

--- entree-app/src/app/app-routing.module.ts
+++ entree-app/src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgetpassComponent } from './forgetpass/forgetpass.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'dashboard', component:DashboardComponent},
-  {path:'login', component:HomeComponent},
-  {path:'signup', component:HomeComponent},
-  {path:'forgetpassword', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signup', component:RegisterComponent},
+  {path:'forgetpassword', component:ForgetpassComponent},
   {path:'cart', component:HomeComponent}
 ];
 
