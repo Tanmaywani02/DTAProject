d7d8fe0d7df3b147a9a1877bdc75d64a08147686
Durgesh-Vishwakarma <vishwakarmadurgesh21@gmail.com>
2024-10-03 14:23:38 +0530

    wishlist api added

--- entree-app/src/app/recommend/recommend.component.ts
+++ entree-app/src/app/recommend/recommend.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,21 +9,40 @@ import { Component } from '@angular/core';
 export class RecommendComponent {
   constructor(private _http:HttpClient){
     this.getrecommend();
+    this.getList();
   }
   recommend = new Array<Recommend>();
+  temp:any={}
   url='http://localhost:3000/recommend';
+  urlList = 'http://localhost:3000/list';
   getrecommend(){
     this._http.get<Recommend[]>(this.url)
       .subscribe(response=>this.recommend=response);
   }
+  getList(){
+    this._http.get<WishList>(`${this.urlList}/${localStorage.getItem("id")}`).subscribe(data=>this.temp=data);
+
+  }
+  addToList(id:string){
+    var prods = this.temp.products;
+    prods.push(id);
+    var data={"id":this.temp.id, "products": prods}
+    this._http.put(`${this.urlList}/${localStorage.getItem("id")}`,data).subscribe();
+  }
+
+
 }
 
 class Recommend{
-  id:number=0;
+  id:string='';
   title:string='';
   image:string='';
   description:string='';
   price:number=0;
   isavailable:boolean=false;
   quantity:number=0;
+}
+class WishList{
+  id:string='';
+  products:any=[];
 }
--- entree-app/src/app/wishlist/wishlist.component.ts
+++ entree-app/src/app/wishlist/wishlist.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,6 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./wishlist.component.css']
 })
 export class WishlistComponent {
+  url='http://localhost:3000/recommend';
+  urlList = 'http://localhost:3000/list';
+  recommend = new Array<Recommend>();
+  temp:any={}
+  wishlistdetails = new Array<WishListDetails>();
+  constructor(private _http: HttpClient){
+    this.getrecommend();
+    this.getList();
+    // this.getListDetails();
+  }
+
+  getrecommend(){
+    this._http.get<Recommend[]>(this.url)
+      .subscribe(response=>this.recommend=response);
+  }
+  getList(){
+    this._http.get<WishList>(`${this.urlList}/${localStorage.getItem("id")}`).subscribe(data=>this.temp=data);
+
+  }
+
+  getListDetails(){
+    var tempList = this.temp.products;
+    for(var i of tempList){
+      this._http.get<WishListDetails>(`${this.url}/${i}`).subscribe(data=>this.wishlistdetails.push(data))
+    }
+    console.log(this.wishlistdetails)
+  }
+
   isVisible = true;
 
   openButton() {
@@ -26,4 +55,25 @@ export class WishlistComponent {
     }
    
   }
+}
+
+
+class Recommend{
+  id:string='';
+  title:string='';
+  image:string='';
+  description:string='';
+  price:number=0;
+  isavailable:boolean=false;
+  quantity:number=0;
+}
+class WishList{
+  id:string='';
+  products:any=[];
+}
+
+class WishListDetails{
+  image:string='';
+  title:string='';
+  price:number=0;
 }
