db675e3e7e81c9d5e1f8ace916b133cb813a7192
SuvarnaSuguna16 <20kq1a0424ece@gmail.com>
2024-10-13 16:14:21 +0530

    Login State updated in Header

--- react-entree/src/App.js
+++ react-entree/src/App.js
@@ -1,19 +1,21 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-import Footer from './components/Footer';
+//import Footer from './components/Footer';
 //import ReactDOM from 'react-dom';
 //import { BrowserRouter as Router } from 'react-router-dom';
-
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { LoginPage } from './components/Header1';
 function App() {
   return (
     <div className="content">
         <Header/>
         <br/><br/><br/>
-        <h1 className='text-center'>ECommerce App </h1>
+        {/* <h1 className='text-center'>ECommerce App </h1> */}
         <br/><br/><br/>
         {/* <Footer/> */}
     </div>
+
   );
 }
 
--- react-entree/src/components/Header.jsx
+++ react-entree/src/components/Header.jsx
@@ -5,78 +5,155 @@ import loginIcon from '../images/log-in.svg';
 import bookmarkIcon from '../images/bookmark.svg';
 import cartIcon from '../images/shopping-cart.svg';
 import '../css/Header.css';
+import userIcon from '../images/user-icon.svg';
 
-const HeaderComponent = () => {
-  //const navigate = useNavigate();
+export function Header(){
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [showLoginForm, setShowLoginForm] = useState(false);
 
-  const login = () => {
-    if (localStorage.getItem("id") !== undefined) {
-      localStorage.clear();
-      //navigate('/');
-      alert("Logout Successful!!");
-    } else {
-      //navigate('/login');
-    }
-  };
+    const users =[
+        { id: 1001, username: 'user1@gmail.com', password: '123' },
+        { id: 1002, username: 'user2@gmail.com', password: '234' },
+        { id: 1003, username: 'user3@gmail.com', password: '345' },
+        { id: 1004, username: 'user4@gmail.com', password: '456' },
+        { id: 1005, username: 'user5@gmail.com', password: '567' }
+    ]
 
+    const errors = {
+        uname: "Invalid username",
+        pass: "Invalid password"
+      };
 
-  return (
-    <header id="header">
-      <div className="container-fluid topbar">
-        <nav className="container d-flex justify-content-between pt-2 pb-2">
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    
+        var { uname, pass } = document.forms[0];
+        const userData = users.find((user) => user.username === uname.value);
+        if (userData) {
+            if (userData.password !== pass.value) {
+                setErrorMessages({ name: "pass", message: errors.pass });
+            } else {
+                setIsSubmitted(true);
+                setIsLoggedIn(true);
+                setUser(userData);
+                setShowLoginForm(false);
+            }
+            } else {
+              setErrorMessages({ name: "uname", message: errors.uname });
+            }
+          };
+        
+          const handleLogout = () => {
+            setIsLoggedIn(false);
+            setUser(null);
+            setIsSubmitted(false);
+          };
+
+          const renderErrorMessage = (name) =>
+            name === errorMessages.name && (
+              <div className="error">{errorMessages.message}</div>
+            );
+
+            const renderForm = (
+                <div>
+                  <form onSubmit={handleSubmit}>
+                    <div className="input-container mb-3">
+                      <label className='form-label text-primary'>Username </label><br/>
+                      <input type="text" name="uname" className="form-control" required />
+                      {renderErrorMessage("uname")}
+                    </div>
+                    <div className="input-container">
+                      <label className='form-label text-primary'>Password </label>
+                      <input type="password" name="pass" className="form-control" required /><br/>
+                      <br/>
+                      {renderErrorMessage("pass")}
+                    </div>
+                    <div className="button-container text-center">
+                      <button className="btn btn-primary" type="submit" >Submit</button>
+                    </div>
+                  </form>
+                </div>
+              );
+
+    return (
+        <div>
+          <header id="header">
           
-          <a className="navbar-brand" href="/#">
-            <img src={logo} alt="Logo" width="150" height="50" />
-          </a>
+            <div className="container-fluid topbar">
+              <nav className="container d-flex justify-content-between pt-2 pb-2">
+                
+                <a className="navbar-brand" href="/#">
+                  <img src={logo} alt="Logo" width="150" height="50" />
+                </a>
+      
+                <div id="searchdiv" className="d-none d-sm-none d-md-flex">
+      
+                  <input type="search" name="search" id="searchbar" placeholder="What can we help you Find?" />
+                </div>
+      
+                <div id="iconsdiv" className="iconsdiv">
+                {isLoggedIn ? (
+                    <div className="dropdown">
+                        <button className="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">                    <img src={userIcon} alt="User" width="40" height="20" />
+                        </button>
 
-          <div id="searchdiv" className="d-none d-sm-none d-md-flex">
-
-            <input type="search" name="search" id="searchbar" placeholder="What can we help you Find?" />
-          </div>
-
-          <div id="iconsdiv" className="iconsdiv">
-
-            <a href="/#" onClick={login}>
-              <img src={loginIcon} alt="icon" width="40" height="20" />
-            </a>
-
-            <a href="/wishlist">
-              <img src={bookmarkIcon} alt="icon" width="40" height="20" />
-            </a>
-
-            <a href="/#" >
-              <img src={cartIcon} alt="icon" width="40" height="20" />
-            </a>
-            
-          </div>
-        </nav>
-      </div>
-      <nav className="navbar navbar-expand-md" id="menu">
-        <button className="navbar-toggler toggler_btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle Navigation">
-          <span className="navbar-toggler-icon custom-toggler"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul className="navbar-nav me-auto m-3 m-md-0 mb-lg-0 nav-list">
-            <li className="nav-item">
-              <a className="nav-link" href="#recommend">RECOMMENDATION</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#topcat">TOP CATEGORY</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#newitems">NEW ITEMS</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#itemleft">ITEMS LEFT</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#exclusivebrands">EXCLUSIVE BRANDS</a>
-            </li>
-          </ul>
+                        <ul className='dropdown-menu'>
+                            <li><button className="dropdown-item">My Profile</button></li>
+                            <li><button className="dropdown-item" onClick={handleLogout}>Logout</button></li> 
+                        </ul>
+                    </div>
+                 ) : (
+                    <img src={loginIcon} alt="Login" width="40" height="20" onClick={() => setShowLoginForm(!showLoginForm)} />
+                )}  
+                  <a href="/wishlist">
+                    <img src={bookmarkIcon} alt="icon" width="40" height="20" />
+                  </a>
+      
+                  <a href="/#" >
+                    <img src={cartIcon} alt="icon" width="40" height="20" />
+                  </a>
+                  
+                </div>
+              </nav>
+                
+                {isLoggedIn? alert("User is successfully logged in"):<div></div>}
+            </div>
+            <nav className="navbar navbar-expand-md" id="menu">
+              <button className="navbar-toggler toggler_btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle Navigation">
+                <span className="navbar-toggler-icon custom-toggler"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul className="navbar-nav me-auto m-3 m-md-0 mb-lg-0 nav-list">
+                  <li className="nav-item">
+                    <a className="nav-link" href="#recommend">RECOMMENDATION</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#topcat">TOP CATEGORY</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#newitems">NEW ITEMS</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#itemleft">ITEMS LEFT</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#exclusivebrands">EXCLUSIVE BRANDS</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+            <br/><br/>
+            {showLoginForm && !isLoggedIn && (
+                    <div className="login-form">
+                    {renderForm}
+                    </div>
+                )}
+          </header>
         </div>
-      </nav>
-    </header>
-  );
-};
+    )
+}
 
-export default HeaderComponent;
+export default Header;
